0edb35dd74b15d9159d8fe4cb549c7427becd19a
Seungwon Lee <seungwon91@gmail.com>
2017-09-17 14:23:01 -0400

    Update ffnn_baseline_model.py

--- ffnn_baseline_model.py
+++ ffnn_baseline_model.py
@@ -77,31 +77,12 @@ class FFNN_minibatch():
             self.test_layers, _ = new_fc_net(test_x_batch, self.layers_size, params=self.param, output_type=None)
 
         #### functions of model
-        if classification and self.layers_size[-1]>1:
-            self.train_eval = tf.nn.softmax(self.train_layers[-1])
-            self.valid_eval = tf.nn.softmax(self.valid_layers[-1])
-            self.test_eval = tf.nn.softmax(self.test_layers[-1])
+        tr_eval, v_eval, test_eval, tr_loss, v_loss, test_loss, tr_acc, v_acc, test_acc = mtl_model_output_functions(models=[[self.train_layers], [self.valid_layers], [self.test_layers]], y_batches=[[train_y_batch], [valid_y_batch], [test_y_batch]], num_tasks=1, dim_output=self.layers_size[-1], classification=classification)
 
-            self.train_loss = tf.nn.softmax_cross_entropy_with_logits(labels=train_y_batch, logits=self.train_layers[-1])
-            self.valid_loss = tf.nn.softmax_cross_entropy_with_logits(labels=valid_y_batch, logits=self.valid_layers[-1])
-            self.test_loss = tf.nn.softmax_cross_entropy_with_logits(labels=test_y_batch, logits=self.test_layers[-1])
-
-            self.train_accuracy = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(self.train_eval, 1), tf.argmax(train_y_batch, 1)), tf.float32))
-            self.valid_accuracy = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(self.valid_eval, 1), tf.argmax(valid_y_batch, 1)), tf.float32))
-            self.test_accuracy = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(self.test_eval, 1), tf.argmax(test_y_batch, 1)), tf.float32))
-        else:
-            self.train_eval = self.train_layers[-1]
-            self.valid_eval = self.valid_layers[-1]
-            self.test_eval = self.test_layers[-1]
-
-            self.train_loss = 2.0* tf.nn.l2_loss(self.train_eval-train_y_batch)
-            self.valid_loss = 2.0* tf.nn.l2_loss(self.valid_eval-valid_y_batch)
-            self.test_loss = 2.0* tf.nn.l2_loss(self.test_eval-test_y_batch)
-
-            if classification:
-                self.train_accuracy = tf.reduce_sum(tf.cast(tf.equal((self.train_eval>0.5), (train_y_batch>0.5)), tf.float32))
-                self.valid_accuracy = tf.reduce_sum(tf.cast(tf.equal((self.valid_eval>0.5), (valid_y_batch>0.5)), tf.float32))
-                self.test_accuracy = tf.reduce_sum(tf.cast(tf.equal((self.test_eval>0.5), (test_y_batch>0.5)), tf.float32))
+        self.train_eval, self.valid_eval, self.test_eval = tr_eval[0], v_eval[0], test_eval[0]
+        self.train_loss, self.valid_loss, self.test_loss = tr_loss[0], v_loss[0], test_loss[0]
+        if not (tr_acc is None):
+            self.train_accuracy, self.valid_accuracy, self.test_accuracy = tr_acc[0], v_acc[0], test_acc[0]
 
         #self.update = tf.train.RMSPropOptimizer(learning_rate=self.learn_rate).minimize(self.train_loss)
         self.update = tf.train.RMSPropOptimizer(learning_rate=self.learn_rate/(1.0+self.epoch/self.learn_rate_decay)).minimize(self.train_loss)
@@ -121,14 +102,14 @@ class MTL_FFNN_minibatch():
         self.batch_size = batch_size
 
         #### data
-        self.data = tflized_data(data_list, do_MTL=True, num_tasks=num_tasks)
+        self.data = tflized_data(data_list, do_MTL=True, num_tasks=self.num_tasks)
 
         #### placeholder of model
         self.data_index = tf.placeholder(dtype=tf.int32)
         self.epoch = tf.placeholder(dtype=tf.float32)
 
         #### mini-batch for training/validation/test data
-        train_x_batch, train_y_batch, valid_x_batch, valid_y_batch, test_x_batch, test_y_batch = minibatched_data(self.data, self.batch_size, self.data_index, do_MTL=True, num_tasks=num_tasks)
+        train_x_batch, train_y_batch, valid_x_batch, valid_y_batch, test_x_batch, test_y_batch = minibatched_data(self.data, self.batch_size, self.data_index, do_MTL=True, num_tasks=self.num_tasks)
 
         #### layers of model for train data
         self.train_models = []
@@ -174,107 +155,78 @@ class MTL_FFNN_minibatch():
 class MTL_FFNN_HPS_minibatch():
     def __init__(self, num_tasks, dim_layers, batch_size, learning_rate, learning_rate_decay, data_list, classification=False):
         self.num_tasks = num_tasks
-        self.num_layers = [len(dim_layers[0])-1, (len(dim_layers[1])//self.num_tasks)]
+        #self.num_layers = [len(dim_layers[0])-1, (len(dim_layers[1])//self.num_tasks)]
+        #self.shared_layers_size = dim_layers[0]      ## include input dimension
+        #self.independent_layers_size = dim_layers[1] ## assume depth of task-specific nets are same
+
         self.shared_layers_size = dim_layers[0]
-        self.independent_layers_size = dim_layers[1]
+        self.task_specific_layers_size = dim_layers[1]
+        self.num_layers = [len(self.shared_layers_size)-1] + [len(self.task_specific_layers_size[x]) for x in range(self.num_tasks)]
+
         self.learn_rate = learning_rate
         self.learn_rate_decay = learning_rate_decay
         self.batch_size = batch_size
 
         #### data
-        self.data = tflized_data(data_list, do_MTL=True, num_tasks=num_tasks)
+        self.data = tflized_data(data_list, do_MTL=True, num_tasks=self.num_tasks)
 
         #### placeholder of model
         self.data_index = tf.placeholder(dtype=tf.int32)
         self.epoch = tf.placeholder(dtype=tf.float32)
 
         #### mini-batch for training/validation/test data
-        train_x_batch, train_y_batch, valid_x_batch, valid_y_batch, test_x_batch, test_y_batch = minibatched_data(self.data, self.batch_size, self.data_index, do_MTL=True, num_tasks=num_tasks)
+        train_x_batch, train_y_batch, valid_x_batch, valid_y_batch, test_x_batch, test_y_batch = minibatched_data(self.data, self.batch_size, self.data_index, do_MTL=True, num_tasks=self.num_tasks)
 
         #### layers of model for train data
-        self.train_models, self.param = [], []
-        for layer_cnt in range(self.num_layers[0]):
-            layer_of_model = []
-            for task_cnt in range(self.num_tasks):
-                if task_cnt == 0 and layer_cnt == 0:
-                    layer_tmp, para_tmp = new_fc_layer(train_x_batch[task_cnt], self.shared_layers_size[layer_cnt], self.shared_layers_size[layer_cnt+1])
-                    self.param = self.param + para_tmp
-                elif layer_cnt == 0:
-                    layer_tmp, _ = new_fc_layer(train_x_batch[task_cnt], self.shared_layers_size[layer_cnt], self.shared_layers_size[layer_cnt+1], weight=self.param[2*layer_cnt], bias=self.param[2*layer_cnt+1])
-                elif task_cnt == 0:
-                    layer_tmp, para_tmp = new_fc_layer(self.train_models[layer_cnt-1][task_cnt], self.shared_layers_size[layer_cnt], self.shared_layers_size[layer_cnt+1])
-                    self.param = self.param + para_tmp
-                else:
-                    layer_tmp, _ = new_fc_layer(self.train_models[layer_cnt-1][task_cnt], self.shared_layers_size[layer_cnt], self.shared_layers_size[layer_cnt+1], weight=self.param[2*layer_cnt], bias=self.param[2*layer_cnt+1])
-                layer_of_model.append(layer_tmp)
-            self.train_models.append(layer_of_model)
+        self.train_models, self.specific_param = [], []
+        for task_cnt in range(self.num_tasks):
+            #### generate network common to tasks
+            if task_cnt == 0:
+                shared_model_tmp, self.shared_param = new_fc_net(train_x_batch[task_cnt], self.shared_layers_size, params=None, output_type='same')
+            else:
+                shared_model_tmp, _ = new_fc_net(train_x_batch[task_cnt], self.shared_layers_size, params=self.shared_param, output_type='same')
 
-        for layer_cnt in range(self.num_layers[1]):
-            layer_of_model = []
-            for task_cnt in range(self.num_tasks):
-                if layer_cnt == 0:
-                    layer_tmp, para_tmp = new_fc_layer(self.train_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.shared_layers_size[-1], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt])
-                elif layer_cnt == self.num_layers[1]-1:
-                    layer_tmp, para_tmp = new_fc_layer(self.train_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.independent_layers_size[(layer_cnt-1)*self.num_tasks+task_cnt], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt], activation_fn=None)
-                else:
-                    layer_tmp, para_tmp = new_fc_layer(self.train_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.independent_layers_size[(layer_cnt-1)*self.num_tasks+task_cnt], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt])
-                layer_of_model.append(layer_tmp)
-                self.param = self.param+para_tmp
-            self.train_models.append(layer_of_model)
-        #self.train_eval = self.train_models[-1]
+            #### generate task-dependent network
+            ts_layer_dim = [self.shared_layers_size[-1]] + self.task_specific_layers_size[task_cnt]
+            if classification:
+                specific_model_tmp, ts_params = new_fc_net(shared_model_tmp[-1], ts_layer_dim, params=None, output_type='classification')
+            else:
+                specific_model_tmp, ts_params = new_fc_net(shared_model_tmp[-1], ts_layer_dim, params=None, output_type=None)
+
+            self.train_models.append(shared_model_tmp + specific_model_tmp)
+            self.specific_param.append(ts_params)
+        self.param = self.shared_param + sum(self.specific_param, [])
 
         #### layers of model for validation data
         self.valid_models = []
-        for layer_cnt in range(self.num_layers[0]):
-            layer_of_model = []
-            for task_cnt in range(self.num_tasks):
-                if layer_cnt == 0:
-                    layer_tmp, _ = new_fc_layer(valid_x_batch[task_cnt], self.shared_layers_size[layer_cnt], self.shared_layers_size[layer_cnt+1], weight=self.param[2*layer_cnt], bias=self.param[2*layer_cnt+1])
-                else:
-                    layer_tmp, _ = new_fc_layer(self.valid_models[layer_cnt-1][task_cnt], self.shared_layers_size[layer_cnt], self.shared_layers_size[layer_cnt+1], weight=self.param[2*layer_cnt], bias=self.param[2*layer_cnt+1])
-                layer_of_model.append(layer_tmp)
-            self.valid_models.append(layer_of_model)
+        for task_cnt in range(self.num_tasks):
+            #### generate network common to tasks
+            shared_model_tmp, _ = new_fc_net(valid_x_batch[task_cnt], self.shared_layers_size, params=self.shared_param, output_type='same')
 
-        for layer_cnt in range(self.num_layers[1]):
-            layer_of_model = []
-            for task_cnt in range(self.num_tasks):
-                if layer_cnt == 0:
-                    layer_tmp, _ = new_fc_layer(self.valid_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.shared_layers_size[-1], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt], weight=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)], bias=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)+1])
-                elif layer_cnt == self.num_layers[1]-1:
-                    layer_tmp, _ = new_fc_layer(self.valid_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.independent_layers_size[(layer_cnt-1)*self.num_tasks+task_cnt], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt], activation_fn=None, weight=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)], bias=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)+1])
-                else:
-                    layer_tmp, _ = new_fc_layer(self.valid_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.independent_layers_size[(layer_cnt-1)*self.num_tasks+task_cnt], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt], weight=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)], bias=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)+1])
-                layer_of_model.append(layer_tmp)
-            self.valid_models.append(layer_of_model)
-        #self.valid_eval = self.valid_models[-1]
+            #### generate task-dependent network
+            ts_layer_dim = [self.shared_layers_size[-1]] + self.task_specific_layers_size[task_cnt]
+            if classification:
+                specific_model_tmp, _ = new_fc_net(shared_model_tmp[-1], ts_layer_dim, params=self.specific_param[task_cnt], output_type='classification')
+            else:
+                specific_model_tmp, _ = new_fc_net(shared_model_tmp[-1], ts_layer_dim, params=self.specific_param[task_cnt], output_type=None)
+            self.valid_models.append(shared_model_tmp + specific_model_tmp)
 
         #### layers of model for test data
         self.test_models = []
-        for layer_cnt in range(self.num_layers[0]):
-            layer_of_model = []
-            for task_cnt in range(self.num_tasks):
-                if layer_cnt == 0:
-                    layer_tmp, _ = new_fc_layer(test_x_batch[task_cnt], self.shared_layers_size[layer_cnt], self.shared_layers_size[layer_cnt+1], weight=self.param[2*layer_cnt], bias=self.param[2*layer_cnt+1])
-                else:
-                    layer_tmp, _ = new_fc_layer(self.test_models[layer_cnt-1][task_cnt], self.shared_layers_size[layer_cnt], self.shared_layers_size[layer_cnt+1], weight=self.param[2*layer_cnt], bias=self.param[2*layer_cnt+1])
-                layer_of_model.append(layer_tmp)
-            self.test_models.append(layer_of_model)
+        for task_cnt in range(self.num_tasks):
+            #### generate network common to tasks
+            shared_model_tmp, _ = new_fc_net(test_x_batch[task_cnt], self.shared_layers_size, params=self.shared_param, output_type='same')
 
-        for layer_cnt in range(self.num_layers[1]):
-            layer_of_model = []
-            for task_cnt in range(self.num_tasks):
-                if layer_cnt == 0:
-                    layer_tmp, _ = new_fc_layer(self.test_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.shared_layers_size[-1], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt], weight=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)], bias=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)+1])
-                elif layer_cnt == self.num_layers[1]-1:
-                    layer_tmp, _ = new_fc_layer(self.test_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.independent_layers_size[(layer_cnt-1)*self.num_tasks+task_cnt], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt], activation_fn=None, weight=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)], bias=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)+1])
-                else:
-                    layer_tmp, _ = new_fc_layer(self.test_models[self.num_layers[0]+layer_cnt-1][task_cnt], self.independent_layers_size[(layer_cnt-1)*self.num_tasks+task_cnt], self.independent_layers_size[layer_cnt*self.num_tasks+task_cnt], weight=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)], bias=self.param[2*(self.num_layers[0]+self.num_tasks*layer_cnt+task_cnt)+1])
-                layer_of_model.append(layer_tmp)
-            self.test_models.append(layer_of_model)
-        #self.test_eval = self.test_models[-1]
+            #### generate task-dependent network
+            ts_layer_dim = [self.shared_layers_size[-1]] + self.task_specific_layers_size[task_cnt]
+            if classification:
+                specific_model_tmp, _ = new_fc_net(shared_model_tmp[-1], ts_layer_dim, params=self.specific_param[task_cnt], output_type='classification')
+            else:
+                specific_model_tmp, _ = new_fc_net(shared_model_tmp[-1], ts_layer_dim, params=self.specific_param[task_cnt], output_type=None)
+            self.test_models.append(shared_model_tmp + specific_model_tmp)
 
         #### functions of model
-        self.train_eval, self.valid_eval, self.test_eval, self.train_loss, self.valid_loss, self.test_loss, self.train_accuracy, self.valid_accuracy, self.test_accuracy = mtl_model_output_functions([self.train_models, self.valid_models, self.test_models], [train_y_batch, valid_y_batch, test_y_batch], num_tasks, self.layers_size[-1], classification)
+        self.train_eval, self.valid_eval, self.test_eval, self.train_loss, self.valid_loss, self.test_loss, self.train_accuracy, self.valid_accuracy, self.test_accuracy = mtl_model_output_functions([self.train_models, self.valid_models, self.test_models], [train_y_batch, valid_y_batch, test_y_batch], num_tasks, self.task_specific_layers_size[0][-1], classification)
 
         self.update = [tf.train.RMSPropOptimizer(learning_rate=self.learn_rate/(1.0+self.epoch/self.learn_rate_decay)).minimize(self.train_loss[x]) for x in range(self.num_tasks)]
 
@@ -299,7 +251,7 @@ class MTL_FFNN_tensorfactor_minibatch():
         self.batch_size = batch_size
 
         #### data
-        self.data = tflized_data(data_list, do_MTL=True, num_tasks=num_tasks)
+        self.data = tflized_data(data_list, do_MTL=True, num_tasks=self.num_tasks)
 
         #### placeholder of model
         self.data_index = tf.placeholder(dtype=tf.int32)
@@ -309,7 +261,7 @@ class MTL_FFNN_tensorfactor_minibatch():
         l1_reg, l2_reg = tf.contrib.layers.l1_regularizer(scale=self.l1_reg_scale), tf.contrib.layers.l2_regularizer(scale=self.l2_reg_scale)
 
         #### mini-batch for training/validation/test data
-        train_x_batch, train_y_batch, valid_x_batch, valid_y_batch, test_x_batch, test_y_batch = minibatched_data(self.data, self.batch_size, self.data_index, do_MTL=True, num_tasks=num_tasks)
+        train_x_batch, train_y_batch, valid_x_batch, valid_y_batch, test_x_batch, test_y_batch = minibatched_data(self.data, self.batch_size, self.data_index, do_MTL=True, num_tasks=self.num_tasks)
 
         #### layers of model for train data
         self.train_models, self.KB_param, self.TS_param = [], [], []
